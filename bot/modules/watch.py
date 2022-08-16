8ebcb4e9d4f568d2d2681b41f5715df976fe1760
alienxsup <111292635+alienxsup@users.noreply.github.com>
2022-08-15 22:46:08 -0700

    Update watch.py

--- bot/modules/watch.py
+++ bot/modules/watch.py
@@ -30,7 +30,7 @@ def _watch(bot, message, isZip=False, isLeech=False, multi=0):
             b_uname = bot_d.username
             uname = f'<a href="tg://user?id={message.from_user.id}">{message.from_user.first_name}</a>'
             botstart = f"http://t.me/{b_uname}"
-            buttons.buildbutton("Click Here to Start Me", f"{botstart}")
+            buttons.buildbutton("ʟᴇᴛs ɢᴇᴛ sᴛᴀʀᴛᴇᴅ", f"{botstart}")
             startwarn = f"ᴀʜʜ {uname},\n\n<b>sᴇᴇᴍs ʟɪᴋᴇ ʏᴏᴜ ʜᴀᴠᴇɴᴛ ᴛᴀʟᴋᴇᴅ ᴍᴇ ɪɴ ᴘʀɪᴠᴀᴛᴇ [ᴘᴍ] ʏᴇᴛ.</b>\n\n" \
                         f"ғʀᴏᴍ ɴᴏᴡ ᴏɴ ɪ ᴡɪʟʟ ɢɪᴠᴇ ʟɪɴᴋ ᴀɴᴅ ʟᴇᴇᴄʜᴇᴅ ғɪʟᴇs ɪɴ ᴘᴍ ᴀɴᴅ ʟᴏɢs ᴏɴʟʏ."
             message = sendMarkup(startwarn, bot, message, InlineKeyboardMarkup(buttons.build_menu(2)))
