b3419823c6cd9799c67235b8fb9159bba9a95684
alienxsup <111292635+alienxsup@users.noreply.github.com>
2022-08-15 22:44:15 -0700

    Update search.py

--- bot/modules/search.py
+++ bot/modules/search.py
@@ -54,25 +54,25 @@ def torser(update, context):
     if SEARCH_API_LINK is  None and SEARCH_PLUGINS is None:
         sendMessage("No API link or search PLUGINS added for this function", context.bot, update.message)
     elif len(key) == 1 and SEARCH_API_LINK is None:
-        sendMessage("Send a search key along with command", context.bot, update.message)
+        sendMessage("sᴇɴᴅ ᴀ sᴇᴀʀᴄʜ ᴋᴇʏ ᴀʟᴏɴɢ ᴡɪᴛʜ ᴄᴏᴍᴍᴀɴᴅ", context.bot, update.message)
     elif len(key) == 1:
         buttons.sbutton('Trending', f"torser {user_id} apitrend")
         buttons.sbutton('Recent', f"torser {user_id} apirecent")
         buttons.sbutton("Cancel", f"torser {user_id} cancel")
         button = InlineKeyboardMarkup(buttons.build_menu(2))
-        sendMarkup("Send a search key along with command", context.bot, update.message, button)
+        sendMarkup("sᴇɴᴅ ᴀ sᴇᴀʀᴄʜ ᴋᴇʏ ᴀʟᴏɴɢ ᴡɪᴛʜ ᴄᴏᴍᴍᴀɴᴅ", context.bot, update.message, button)
     elif SEARCH_API_LINK is not None and SEARCH_PLUGINS is not None:
-        buttons.sbutton('Api', f"torser {user_id} apisearch")
-        buttons.sbutton('Plugins', f"torser {user_id} plugin")
-        buttons.sbutton("Cancel", f"torser {user_id} cancel")
+        buttons.sbutton('ᴀᴘɪ', f"torser {user_id} apisearch")
+        buttons.sbutton('ᴘʟᴜɢɪɴs', f"torser {user_id} plugin")
+        buttons.sbutton("ᴄᴀɴᴄᴇʟ", f"torser {user_id} cancel")
         button = InlineKeyboardMarkup(buttons.build_menu(2))
-        sendMarkup('Choose tool to search:', context.bot, update.message, button)
+        sendMarkup('ᴄʜᴏᴏsᴇ ᴛᴏᴏʟ ᴛᴏ sᴇᴀʀᴄʜ:', context.bot, update.message, button)
     elif SEARCH_API_LINK is not None and SEARCH_PLUGINS is None:
         button = _api_buttons(user_id, "apisearch")
-        sendMarkup('Choose site to search:', context.bot, update.message, button)
+        sendMarkup('ᴄʜᴏᴏsᴇ sɪᴛᴇ ᴛᴏ sᴇᴀʀᴄʜ:', context.bot, update.message, button)
     elif SEARCH_API_LINK is None and SEARCH_PLUGINS is not None:
         button = _plugin_buttons(user_id)
-        sendMarkup('Choose site to search:', context.bot, update.message, button)
+        sendMarkup('ᴄʜᴏᴏsᴇ sɪᴛᴇ ᴛᴏ sᴇᴀʀᴄʜ:', context.bot, update.message, button)
 
 def torserbut(update, context):
     query = update.callback_query
