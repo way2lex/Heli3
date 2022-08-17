660293f1c1d36677212088e92fdefb84ce9d7ec9
alienxsup <111292635+alienxsup@users.noreply.github.com>
2022-08-16 20:02:33 -0700

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -72,7 +72,7 @@ class TelegraphHelper:
                     nxt_page += 1
             self.edit_page(
                 path = path[prev_page],
-                title = 'Mirror-leech-bot Torrent Search',
+                title = 'sᴘʏ x ᴍɪʀʀᴏʀ ᴛᴏʀʀᴇɴᴛ sᴇᴀʀᴄʜ',
                 content=content
             )
         return
